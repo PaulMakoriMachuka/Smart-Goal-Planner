ea23b029a4e65119c91c7ab32562fc250a4c669a
PaulMakoriMachuka <paulmachukamakori@gmail.com>
2025-07-22 13:11:29 +0300

    date

--- src/Table.jsx
+++ src/Table.jsx
@@ -30,10 +30,10 @@ function GoalForm({ onAddGoal }) {
     <form onSubmit={handleSubmit} className="mb-4">
       <input name="name" value={formData.name} onChange={handleChange} placeholder="Goal Name" />
       <input name="targetAmount" value={formData.targetAmount} onChange={handleChange} placeholder="Target Amount" />
-      <input name="totalAmount" value={formData.targetAmount} onChange={handleChange} placeholder="Total Amount" />
+      <input name="totalAmount" value={formData.totalAmount} onChange={handleChange} placeholder="Total Amount" />
       <input name="category" value={formData.category} onChange={handleChange} placeholder="Category" />
-      <input name="created" value={formData.deadline} onChange={handleChange} placeholder="Created" />
-      <input name="deadline" value={formData.deadline} onChange={handleChange} placeholder="Deadline" />
+      <input type="date"name="created" value={formData.created} onChange={handleChange} placeholder="Created" />
+      <input type="date" name="deadline" value={formData.deadline} onChange={handleChange} placeholder="Deadline" />
       <button type="submit">Add Goal</button>
     </form>
   );
